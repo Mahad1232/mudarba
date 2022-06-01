43977b11fe9234a816eba30e8014f4ae21060a9d
Mahad1232 <mahadsaad4110@gmail.com>
2022-06-01 13:12:57 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-yykqhi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-yykqhi)
---
+++ src/Home/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Aux from '../../Auxiliary/auxiliary.js';
+
+const Home = () => {
+  return (
+    <Aux>
+      <h1>Hello</h1>
+    </Aux>
+  );
+};
+
+export default Home;
