62c5c93d3acb04455e166102d971f3cd019f92bc
Mahad1232 <mahadsaad4110@gmail.com>
2022-06-03 15:00:34 +0500

    2 files modified

--- src/Home/pages/home.js
+++ src/Home/pages/home.js
@@ -8,7 +8,11 @@ const HomePage = () => {
   return (
     <Aux>
       <section className="header__outer">
-        <h1>Buisness Chain</h1>
+        <h1 className="logo">Buisness Chain</h1>
+        <div className="login">
+          <h3 className="login1">Login</h3>
+          <h3 className="login1">Signup</h3>
+        </div>
       </section>
     </Aux>
   );
