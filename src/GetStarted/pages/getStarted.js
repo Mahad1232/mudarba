222e44d27384ad1aec5acb75f677034bc985bbb4
Mahad1232 <mahadsaad4110@gmail.com>
2022-06-03 17:07:49 +0500

    1 file modified, 1 added

--- src/GetStarted/pages/getStarted.js
+++ src/GetStarted/pages/getStarted.js
@@ -4,10 +4,17 @@ import Aux from '../../Auxiliary/auxiliary.js';
 
 import Nav from '../../Nav/nav';
 
+import './getStarted.css';
+
 const getStartedPage = () => {
   return (
     <Aux>
       <Nav />
+      <section className="getStarted__outer">
+        <div className="getStarted__inner">
+          <h3 className="finPock">FinPocket helps u save and invest </h3>
+        </div>
+      </section>
     </Aux>
   );
 };
