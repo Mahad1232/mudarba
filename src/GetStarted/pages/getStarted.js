92cfea39b92ab344413ab8430071f5450a100325
Mahad1232 <mahadsaad4110@gmail.com>
2022-06-03 17:24:29 +0500

    2 files modified

--- src/GetStarted/pages/getStarted.js
+++ src/GetStarted/pages/getStarted.js
@@ -13,6 +13,12 @@ const getStartedPage = () => {
       <section className="getStarted__outer">
         <div className="getStarted__inner">
           <h3 className="finPock">FinPocket helps u save and invest </h3>
+
+          <div className="outer__signup">
+            <div>Buisness</div>
+            <div>Investor</div>
+          </div>
+          <input placeholder="Buisness Signup" />
         </div>
       </section>
     </Aux>
