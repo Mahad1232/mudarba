1c729b53b89234b8457f753d93b13aeb0db8667d
Mahad1232 <mahadsaad4110@gmail.com>
2022-06-04 11:26:40 +0500

    1 file modified

--- src/GetStarted/components/getStarted.js
+++ src/GetStarted/components/getStarted.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 import GetStartedPage from '../pages/getStarted';
+
+import { useState } from 'react-router-dom';
+
 const getStarted = () => {
+  // const [user, setUser] = useState({
+  //   h1: 'hello'
+  // });
   return <GetStartedPage />;
 };
 
